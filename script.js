cb0bfc5114a798bbc9cc390ac57fd9d6df6c9796
Yousef Zain Aldeen <141151486+1mental@users.noreply.github.com>
2025-02-21 04:43:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener("scroll",function () {
+
+    let header = document.getElementsByClassName('navigation-bar')[0];
+    if (window.scrollY > 0)
+    {
+        header.classList.add('sticky');
+    }
+    else
+    {
+
+        header.classList.remove('sticky');
+    }
+})
+
+
+
+function CloseImageShow()
+{
+    document.getElementById('img-show').style.display = 'none';
+    document.body.style.overflow = 'visible';
+}
+
+
+var certificates = document.querySelectorAll('.Cert-Image');
+var ImagePreview = document.getElementById('image-preview');
+
+certificates.forEach((item) => {
+
+    item.addEventListener('click', function(){
+
+        document.getElementById('image-preview').src = item.src;
+        document.getElementById('img-show').style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+
+    });
+})
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
